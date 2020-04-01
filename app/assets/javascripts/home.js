89b93b528ea6993b3b6461b59df87a8f10600e5b
EC2 Default User <ec2-user@ip-172-31-45-200.ap-northeast-1.compute.internal>
2020-04-01 13:07:03 +0000

    h

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,4 @@
-// [トップ画fadein]
+// // [トップ画fadein]
 $(window).on('turbolinks:load', function(){
     $('.home-index-title h1').fadeIn(2500);
 });
@@ -7,9 +7,9 @@ $(window).on('turbolinks:load', function(){
     $('.home-index-title h2').fadeIn(2500);
 });
 
-// [下からfadein]
-$(window).on('turbolinks:load', window).scroll(function(){
-    $('.content-1-sub h1, .content-1-sub li, .home-index-content-2 h2, .content-2-1-text span, .content-2-1-text p, .content-2-2-text span, .content-2-2-text p, .content-2-3-text span, .content-2-3-text p, .content-2-4-text span, .content-2-4-text p, .content-2-5-text span, .content-2-5-text p').each(function(){
+// // [下からfadein]
+$(document).on('turbolinks:load', window).scroll(function(){
+    $('.content-1-sub h1, .content-1-sub li, .home-index-content-2 h2, .content-2-1-text span, .content-2-1-text p, .content-2-2-text span, .content-2-2-text p, .content-2-3-text span, .content-2-3-text p, .content-2-4-text span, .content-2-4-text p, .content-2-5-text span, .content-2-5-text p, .btn-area-login, .btn-area-sign-up, .btn-area-contact').each(function(){
         let dummy = $(this).offset().top,
         scroll = $(window).scrollTop(),
         windowHeight = $(window).height();
@@ -19,7 +19,7 @@ $(window).on('turbolinks:load', window).scroll(function(){
     });
 });
 
-// [cursolの動き]
+// // [cursolの動き]
 $(function(){
     setTimeout('anime()');
 });
